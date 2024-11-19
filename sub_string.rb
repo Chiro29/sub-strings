3d9ec46dc254a7ca103c9152d793120203ce1898
chiro <80062293+Chiro29@users.noreply.github.com>
2024-11-19 13:39:41 +0100

    feat(project): added the possibility to continue using the programme as long the user wants

--- sub_string.rb
+++ sub_string.rb
@@ -37,58 +37,67 @@ def delete(s, array)
   end
 end
 
-puts "Choose:\n1) Find sub strings\n2) Add a word in the dictionary\n3) Delete a word in the dictionary\n4) Show all words inside the dictionary"
+loop do 
+  puts "Choose:\n1) Find sub strings\n2) Add a word in the dictionary\n3) Delete a word in the dictionary\n4) Show all words inside the dictionary"
 
-loop do
-  choice = gets.chomp.to_i
+  loop do
+    choice = gets.chomp.to_i
 
-  case choice
-    when 1  
-      puts "Write something"
-      string = gets.chomp
-
-      until string =~ /^[a-zA-Z\s!?,.']+$/ do
-        puts "Only letters of the alphabet and space"
+    case choice
+      when 1  
+        puts "Write something"
         string = gets.chomp
-      end
 
-      result = sub_string(string, dictionary)
+        until string =~ /^[a-zA-Z\s!?,.']+$/ do
+          puts "Only letters of the alphabet and space"
+          string = gets.chomp
+        end
 
-      if result.empty?
-        puts "No words found"
-      else 
-        puts result
-      end
-      
-      break
-    when 2
-      puts "Insert the word you want to add in the dictionary\nRemember that adding or deleting a word in the dictionary will only be temporary and will not be saved in the file"
-      string = gets.chomp
+        result = sub_string(string, dictionary)
 
-      until string =~ /^[a-zA-Z]+$/ do
-        puts "Only letters of the alphabet"
+        if result.empty?
+          puts "No words found"
+        else 
+          puts result
+        end
+        
+        break
+      when 2
+        puts "Insert the word you want to add in the dictionary\nRemember that adding or deleting a word in the dictionary will only be temporary and will not be saved in the file"
         string = gets.chomp
-      end
 
-      dictionary = add(string, dictionary)
-      break
-    when 3
-      puts "Insert the word you want to delete in the dictionary\nRemember that adding or deleting a word in the dictionary will only be temporary and will not be saved in the file"
-      string = gets.chomp
+        until string =~ /^[a-zA-Z]+$/ do
+          puts "Only letters of the alphabet"
+          string = gets.chomp
+        end
 
-      until string =~ /^[a-zA-Z]+$/ do
-        puts "Only letters of the alphabet"
+        dictionary = add(string, dictionary)
+        break
+      when 3
+        puts "Insert the word you want to delete in the dictionary\nRemember that adding or deleting a word in the dictionary will only be temporary and will not be saved in the file"
         string = gets.chomp
-      end
 
-      dictionary = delete(string, dictionary)
-      
-      break
-    when 4 
-      puts dictionary
+        until string =~ /^[a-zA-Z]+$/ do
+          puts "Only letters of the alphabet"
+          string = gets.chomp
+        end
 
-      break
-    else
-      puts "You can only choose between 1 to 3\nChoose:\n1) Find sub strings\n2) Add a word in the dictionary\n3) Delete a word in the dictionary\n4) Show all words inside the dictionary"
+        dictionary = delete(string, dictionary)
+        
+        break
+      when 4 
+        puts dictionary
+
+        break
+      else
+        puts "You can only choose between 1 to 3\nChoose:\n1) Find sub strings\n2) Add a word in the dictionary\n3) Delete a word in the dictionary\n4) Show all words inside the dictionary"
+    end
   end
+
+  puts "Do you want to continue using the programme?\nPress any key other than 'n' or enter to continue\n'n' to close"
+  again = gets.chomp.downcase
+
+  if again === "n"
+    break
+  end  
 end
